40a80686d29c80f3e20851d9c490fe59805af53f
Abdullah Basulaib <basulaib@my.yorku.ca>
2019-12-26 03:01:53 -0500

    scraped frame data, next: clean + add to db

--- scraper.py
+++ scraper.py
@@ -4,22 +4,32 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import csv
+import os.path
 
-chars = pd.read_csv('files/chars.csv')
+chars = pd.read_csv('char_id.csv')
 for char in chars.values:
-    print(char[1])
     url = 'http://rbnorway.org/' + char[1] + '-t7-frames/'
     headers = {'Content-type': 'application/json', 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/12.0'}
-    print(url)
     page = requests.get(url, headers=headers)
-    print(page)
     if page.status_code:
         soup = BeautifulSoup(page.content, 'html.parser')
-        h3 = soup.find_all('h3')
-        print(h3)
-        tb = soup.find('tr')
-        print(tb[0].getText())
-        break
+        path = 'data/' + char[1] + '.csv'
+        headers = ['Command', 'Hit level', 'Damage', 'Start up frame', 'Block frame', 'Hit frame', 'Counter hit frame', 'Notes']
+        if not os.path.isfile(path):
+            with open(path, 'a+', newline='', encoding='utf-16') as f:
+                writer = csv.writer(f)
+                writer.writerow(headers)
+                tr = soup.find_all('tr')
+                for row in tr:
+                    td = row.find_all('td')
+                    if len(td) == 0:
+                        continue
+                    td = [x.text.strip() for x in td]
+                    if td[0] == 'Command':
+                        continue
+                    writer.writerow(td)
+            print(char[1] + " done")
+        else:
+            print(char[1] + " csv exists")
     else:
-        print("error")
-        break
+        print("error on " + char[1])
