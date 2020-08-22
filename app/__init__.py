8ecb42ff769935bb084e7a2171bb3fcf37e07749
Abdullah Basulaib <basulaib@my.yorku.ca>
2020-08-22 06:12:41 -0400

    added config and worked init

---
+++ app/__init__.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, current_app
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from config import Config
+import logging
+import os
+from logging.handlers import SMTPHandler, RotatingFileHandler
+
+db = SQLAlchemy()
+migrate = Migrate()
+bootstrap = Bootstrap()
+moment = Moment()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    
+    db.init_app(app)
+    migrate.init_app(app)
+    moment.init_app(app)
+    bootstrap.init_app(app)
+    
+    if not app.debug and not app.testing:
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/T7D.log', maxBytes=10240, backupCount=10)
+            file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s ''[in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('T7D start')
+    
+    return app
+    
+    
---
+++ app/query.py
@@ -0,0 +1,2 @@
+from flask import Flask
+from sqlalchemy import mysql
